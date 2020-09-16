9929d37a6b25ed696e3d4f3f1c4a9507d7ec5dde
rrbalat <matej.balat@reroot.hr>
2020-09-16 15:56:35 +0200

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App()
-{
-  return (
-    <div className="App">
+const API = 'https://hn.algolia.com/api/v1/search?query='
+const DEFAULT_QUERY = 'redux'
 
-    </div>
-  );
+class App extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount()
+  {
+    fetch(API + DEFAULT_QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data.hits }));
+  }
+
+  render()
+  {
+    const { hits } = this.state;
+
+    return (
+      <ul>
+        {hits.map(hit =>
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>
+        )}
+      </ul>
+    );
+  }
 }
 
 export default App;
